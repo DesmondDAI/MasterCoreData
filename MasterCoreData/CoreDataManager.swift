bb978535412b6794323836443aff387ace127ea9
DesmondDAI <dxmlore@gmail.com>
2017-03-25 23:59:42 +0800

    [New] Add core data manager

---
+++ MasterCoreData/CoreDataManager.swift
@@ -0,0 +1,57 @@
+//
+//  CoreDataManager.swift
+//  MasterCoreData
+//
+//  Created by DAIXinming on 25/03/2017.
+//  Copyright © 2017 Xinming DAI. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+final class CoreDataManager {
+    
+    private let modelName: String
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+    private(set) lazy var managedObjectContext: NSManagedObjectContext = {
+        let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = self.persistentStoreCoordinator
+        
+        return managedObjectContext
+    }()
+    
+    private lazy var managedObjectModel: NSManagedObjectModel = {
+        guard let modelURL = Bundle.main.url(forResource: self.modelName, withExtension: "momd") else {
+            fatalError("Unable to find data model")
+        }
+        
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Unable to load data model")
+        }
+        
+        return managedObjectModel
+    }()
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        
+        let fileManager = FileManager.default
+        let storeName = "\(self.modelName).sqlite"
+        
+        let documentsDirectoryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        
+        let persistentStoreURL = documentsDirectoryURL.appendingPathComponent(storeName)
+        
+        do {
+            try persistentStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: persistentStoreURL, options: nil)
+        } catch {
+            fatalError("Unable to load persistent store")
+        }
+        
+        return persistentStoreCoordinator
+    }()
+}
